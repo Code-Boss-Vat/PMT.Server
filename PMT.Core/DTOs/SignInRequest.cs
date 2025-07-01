c10b3015fdba342e2dd43f1416ccdefa41f98190
AkashBadgujar2408 <akashbadgujar1999@gmail.com>
2025-07-01 19:38:49 +0530

    Fixed problem with db entity saving due to createdby and updatedby datetime kind mismatch

--- PMT.Core/DTOs/SignInRequest.cs
+++ PMT.Core/DTOs/SignInRequest.cs
@@ -7,10 +7,11 @@ public class SignInRequest
 {
     [Required(ErrorMessage = "{0} is required.")]
     [DisplayName("User Name")]
-    [Length(5, 20, ErrorMessage = "Length of {0} should be between {1} and {2}.")]
+    [StringLength(20, MinimumLength = 5, ErrorMessage = "Length of {0} should be between {1} and {2}.")]
     public string? UserName { get; set; }
 
     [Required(ErrorMessage = "{0} is required.")]
+    [StringLength(20, MinimumLength = 8, ErrorMessage = "{0} length must be between {2} and {1} characters.")]
     [DataType(DataType.Password)]
     [DisplayName("Password")]
     public string? Password { get; set; }
--- PMT.Core/Entities/EntityBase.cs
+++ PMT.Core/Entities/EntityBase.cs
@@ -1,12 +1,16 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PMT.Core.Entities;
 
 public class EntityBase
 {
     public bool IsActive { get; set; }
     public Guid CreatedBy { get; set; }
+    [Column(TypeName = "timestamp without time zone")]
     public DateTime CreatedOn { get; set; }
     public Guid UpdatedBy { get; set; }
+    [Column(TypeName = "timestamp without time zone")]
     public DateTime UpdatedOn { get; set; }
 }
 
